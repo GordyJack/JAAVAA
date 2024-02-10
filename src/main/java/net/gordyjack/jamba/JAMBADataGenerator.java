d6809b1e213a941ee0c51e1729b5dfc7a88efe08
GordyJack <gordyjack.mail@gmail.com>
2024-02-10 09:26:28 -0600

    Initial Commit

---
+++ src/main/java/net/gordyjack/jamba/JAMBADataGenerator.java
@@ -0,0 +1,11 @@
+package net.gordyjack.jamba;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JAMBADataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
